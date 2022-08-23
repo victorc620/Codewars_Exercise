9f8d1be8d65caa2b9768192a6bde7842280302b2
Victor Chu <victorchu@wejoM-6106.local>
2022-08-23 16:10:27 +0100

    kyu6_FindTheOddInt Completed & Refactored

--- src/main/scala/kyu6/kyu6_FindTheOddInt.scala
+++ src/main/scala/kyu6/kyu6_FindTheOddInt.scala
@@ -2,11 +2,14 @@ package kyu6
 
 object kyu6_FindTheOddInt extends App {
 
-  def findOdd(arrayParameter: Seq[Int]): Int = {
-    val intCount: Map[Int, Int] = arrayParameter.groupBy(identity).view.mapValues(_.size).toMap
-    val intCountOddFilter       = intCount.filter(x => x._2 % 2 != 0)
-    val ans                     = intCountOddFilter.head._1
-    ans
-  }
+  def findOdd(arrayParameter: Seq[Int]): Int =
+    arrayParameter
+      .groupBy(identity)
+      .view
+      .mapValues(_.size)
+      .toMap
+      .filter(x => x._2 % 2 != 0)
+      .head
+      ._1
 
 }
